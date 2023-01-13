f3770b0913cbde5c5c818a9c14430ce8cbac197e
David Chakiryan <davidchak@yandex.ru>
2023-01-13 06:39:48 -0500

    add craco package

---
+++ web/client/craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const resolvePath = p => path.resolve(__dirname, p)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': resolvePath('./src/components'),
+      '@stores': resolvePath('./src/stores'),
+      '@utils': resolvePath('./src/utils'),
+      '@actions': resolvePath('./src/actions'),
+      '@assets': resolvePath('./src/assets')
+    },
+  },
+}
